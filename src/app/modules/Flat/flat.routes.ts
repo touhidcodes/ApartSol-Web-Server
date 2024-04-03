fbda03cc8fda8bd2971d52f9e9d060d5e0c55825
touhidcodes <touhidcodes@gmail.com>
2024-04-03 21:59:40 +0600

    feat: create flat validation added

--- src/app/modules/Flat/flat.routes.ts
+++ src/app/modules/Flat/flat.routes.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import auth from "../../middlewares/auth";
 import { flatControllers } from "./flat.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { flatValidationSchemas } from "./flat.validation";
 
 const router = express.Router();
 
-router.post("/flats", auth(), flatControllers.createFlat);
+router.post(
+  "/flats",
+  auth(),
+  validateRequest(flatValidationSchemas.createFlatSchema),
+  flatControllers.createFlat
+);
 
 export const flatRoutes = router;
---
+++ src/app/modules/Flat/flat.validation.ts
@@ -0,0 +1,57 @@
+import { z } from "zod";
+
+const createFlatSchema = z.object({
+  body: z.object({
+    squareFeet: z
+      .number({
+        required_error:
+          "Square feet is required and must be a positive integer less than 100000",
+      })
+      .int()
+      .min(1)
+      .max(99999),
+    totalBedrooms: z
+      .number({
+        required_error:
+          "Total bedrooms is required and must be a positive integer less than or equal to 4",
+      })
+      .int()
+      .min(1)
+      .max(4),
+    totalRooms: z
+      .number({
+        required_error:
+          "Total rooms is required and must be a positive integer less than or equal to 6",
+      })
+      .int()
+      .min(1)
+      .max(6),
+    utilitiesDescription: z.string({
+      required_error: "Utilities description is required",
+    }),
+    location: z.string({
+      required_error: "Location is required",
+    }),
+    description: z.string({
+      required_error: "Description is required",
+    }),
+    rent: z
+      .number({
+        required_error:
+          "Rent amount is required and must be a positive integer",
+      })
+      .int()
+      .min(1),
+    advanceAmount: z
+      .number({
+        required_error:
+          "Advance amount is required and must be a positive integer",
+      })
+      .int()
+      .min(1),
+  }),
+});
+
+export const flatValidationSchemas = {
+  createFlatSchema,
+};

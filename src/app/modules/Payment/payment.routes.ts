33828b1a5d6ed98cb3c2d1984b4baa545eecda8a
touhidcodes <touhidcodes@gmail.com>
2024-10-15 19:34:49 +0600

    prod: project build

--- src/app/modules/Payment/payment.routes.ts
+++ src/app/modules/Payment/payment.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { paymentController } from "./payment.controller";
 import bodyParser from "body-parser";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.post("/create-payment", paymentController.createPayment);
+router.post(
+  "/create-payment",
+  auth(UserRole.USER),
+  paymentController.createPayment
+);
 router.get("/payment-status/:sessionId", paymentController.getPaymentStatus);
 router.post(
   "/webhook",
